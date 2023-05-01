9f9b744bd14cb65918e5153365de590c2761e756
Ana Carolina Rodrigues <anacarolinarodrigues480@gmail.com>
2023-05-01 19:32:42 -0300

    Add function findwords

--- jogo_caca_palavras.py
+++ jogo_caca_palavras.py
@@ -3,9 +3,7 @@ import random
 from tkinter import messagebox
 from typing import List
 
-
 class Solution:
-
 
     def __init__(self):
         # making the possible moves in movers array
@@ -14,4 +12,25 @@ class Solution:
             [1, 1], [-1, -1], [1, -1], [-1, 1]
         ]
 
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        result = []
+        rows = len(board)
+        cols = len(board[0])
+
+        # making board a global variable
+        self.board = board
+
+        for word in words:
+            for i in range(rows):
+                for j in range(cols):
+                    if self.board[i][j] == word[0]:
+
+                        # making a function findwords to
+                        # find words along with their
+                        # location which inputs the board
+                        # and list of words
+                        if self.dfs(i, j, word[1:], [[False] * cols for _ in range(rows)]):
+                            result.append(f"{word}")
+
+        return result
     
