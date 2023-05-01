c3c48abc9e18ed9a981effa15670b4b4f0093750
Mateus de Almeida <80906504+Mateuszinnn@users.noreply.github.com>
2023-05-01 19:40:08 -0300

    add function dfs

--- jogo_caca_palavras.py
+++ jogo_caca_palavras.py
@@ -34,3 +34,35 @@ class Solution:
 
         return result
     
+    # depth first search for the string, with the
+    # coordinates and a visited array to take care that we
+    # do not overlap the places visited already
+    def dfs(self, x: int, y: int, s: str, vis: List[List[bool]]) -> bool:
+
+        # if string length becomes 0 means string is found
+        if not s:
+            return True
+
+        vis[x][y] = True
+
+        # making a solution boolean to see if we can
+        # perform depth search to find answer
+        sol = False
+
+        # making possible moves
+        for move in self.mover:
+            currX = move[0] + x
+            currY = move[1] + y
+
+            # checking for out of bound areas
+            if 0 <= currX < len(self.board) and 0 <= currY < len(self.board[0]):
+
+                # checking for similarity in the first
+                # letter and the visited array
+                if self.board[currX][currY] == s[0] and not vis[currX][currY]:
+                    if self.dfs(currX, currY, s[1:], vis):
+                        # removing the first letter
+                        # from the string
+                        sol = True
+
+        return sol
